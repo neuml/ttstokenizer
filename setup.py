8701a6fb3569f442524e042e43092a7b367dbe0c
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2022-11-27 17:07:51 -0500

    Initial commit of TTSTokenizer, cleanup unused files, reference original repo

--- setup.py
+++ setup.py
@@ -1,33 +1,23 @@
-#from distutils.core import setup
+# pylint: disable = C0111
 from setuptools import setup
-from codecs import open
-from os import path
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    DESCRIPTION = f.read()
 
 setup(
-  name = 'g2p_en',
-  packages = ['g2p_en'], # this must be the same as the name above
-  version = '2.0.0',
-  description = 'A Simple Python Module for English Grapheme To Phoneme Conversion',
-  long_description=long_description,
-  author = 'Kyubyong Park & Jongseok Kim',
-  author_email = 'kbpark.linguist@gmail.com',
-  url = 'https://github.com/Kyubyong/g2p', # use the URL to the github repo
-  download_url = 'https://github.com/Kyubyong/g2p/archive/1.0.0.tar.gz', # I'll explain this in a second
-  keywords = ['g2p','g2p_en'], # arbitrary keywords
-  classifiers = [],
-  install_requires = [
-    'numpy>=1.13.1',
-    'nltk>=3.2.4',
-    'inflect>=0.3.1',
-    'distance>=0.1.3',
-  ],
-  license='Apache Software License',
-  include_package_data=True
+    name = "ttstokenizer",
+    version = "1.0.0",
+    description = "Tokenizer for Text to Speech (TTS) models",
+    long_description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/neuml/ttstokenizer",
+    license="Apache 2.0: http://www.apache.org/licenses/LICENSE-2.0",
+    packages = ["ttstokenizer"],
+    install_requires = [
+        "anyascii>=0.3.1",
+        "inflect>=0.3.1",
+        "numpy>=1.13.1",
+        "nltk>=3.2.4"
+    ],
+    include_package_data = True
 )
-
